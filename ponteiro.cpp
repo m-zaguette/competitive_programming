a57949cdd32acdb940efcf9ff2196096d89ad45d
Marcelo Zaguette Junior <41178325+zaguette@users.noreply.github.com>
2018-08-22 17:47:43 -0300

    Update ponteiro.cpp

--- ponteiro.cpp
+++ ponteiro.cpp
@@ -1,15 +1,33 @@
 #include<bits/stdc++.h>
+#define s(x) scanf("%i",x)
+#define pb(x) printf("%i ",(*x));
+#define p(x) printf("%i ",x);
 using namespace std;
 int main(){
-	int valor[10],*p_valor, i;
+	int valor[10],*p_valor, i, x=0;
 	
-	for(i = 0 , p_valor = valor; i<10; i++, p_valor++){
-		scanf("%i",p_valor);
-	}
-	for(i = 0 , p_valor = valor; i<10; i++, p_valor++){
-		cout<<(*p_valor)<<" ";
-		if(i==9){
-			cout<<endl;
+	cin>>x;
+	
+	while(x--){
+
+		for(i = 0 , p_valor = valor; i<10; i++, p_valor++){
+			s(p_valor);
+		}
+		
+		cout<<"Posicao da memoria ocupada: "<<endl;
+		for(i = 0 , p_valor = valor; i<10; i++, p_valor++){
+			p(p_valor);
+			if(i==9){
+				cout<<endl;
+			}
+		}
+		
+		cout<<"Valor guardado na posicao da memoria: "<<endl;
+		for(i = 0 , p_valor = valor; i<10; i++, p_valor++){
+			pb(p_valor);
+			if(i==9){
+				cout<<endl;
+			}
 		}
 	}
 }
